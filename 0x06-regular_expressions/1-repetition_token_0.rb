2e6374084ad8f9ea81539e04a4b175110112abf4
Ismael-ux-1 <ismaelabdulahi1@gmail.com>
2023-08-01 15:44:08 -0700

    fixed  Repetition Token #0

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,13 +1,2 @@
 #!/usr/bin/env ruby
-#Ruby script that acccepts one argument and 
-#pass it to a regular expressiion matching method
-def match_school(input)
-  if input.match(/hb?tn/)
-    puts input.scan(/hb?tn/).join
-  else
-    puts ""
-  end
-end
-
-input = ARGV[0]
-match_school(input)
+puts ARGV[0].scan(/hb?tn/).join
