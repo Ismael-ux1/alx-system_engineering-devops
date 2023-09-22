ff8f6428ec3955d0558700e9ead815785b3c5d31
Ismael-ux1 <ismaelabdulahi1@gmail.com>
2023-09-22 16:34:49 -0700

    updated create file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,8 @@
+# Creates a file in the /tmp using Puppet
 file { '/tmp/school':
-  ensure  => file,        # Ensure it's a file
-  mode    => '0744',     # File permission is 0744
-  owner   => 'www-data', # File owner is www-data
-  group   => 'www-data', # File group is www-data
-  content => 'I love Puppet', # File content
+  ensure  => file,
+  mode    => '0744',
+  owner   => 'www-data'
+  group   => 'www-data',
+  content => 'I love Puppet',
 }
