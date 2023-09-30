4f44cbb2daf4434504e664d90d9e845c3d248603
Ismael-ux1 <ismaelabdulahi1@gmail.com>
2023-09-29 19:56:47 -0700

    updated Install Nginx web server (w/ Puppet)

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,60 +1,40 @@
+class nginx {
+  package { 'nginx':
+    ensure => installed,
+  }
 
+  service { 'nginx':
+    ensure     => running,
+    enable     => true,
+    require    => Package['nginx'],
+  }
 
-# Install Nginx web server
-package { 'nginx':
-  ensure => installed,
-}
+  file { '/var/www/html/index.html':
+    ensure  => file,
+    content => 'Hello World!',
+    require => Package['nginx'],
+  }
 
-# Create the Nginx configuration file for the main server block
-file { '/etc/nginx/sites-available/default':
-  ensure  => file,
-  content => "server {
+  file { '/etc/nginx/sites-available/default':
+    ensure  => file,
+    content => '
+server {
     listen 80 default_server;
     listen [::]:80 default_server;
     root /var/www/html;
     index index.html;
 
-    location /redirect_me {
-        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
-    }
-
     location / {
-        try_files $uri $uri/ @default;
+        try_files $uri $uri/ =404;
     }
 
-    location @default {
-        return 200 'Hello World!';
-        add_header Content-Type text/plain;
+    location /redirect_me {
+        return 301 https://www.example.com;
     }
-}\n",
-  require => Package['nginx'],
+}',
+    notify  => Service['nginx'],
+    require => Package['nginx'],
+  }
 }
 
-# Create the HTML file for the main server block
-file { '/var/www/html/index.html':
-  ensure  => file,
-  content => 'Hello World!',
-}
-
-# Create a symlink for the server block to enable it
-file { '/etc/nginx/sites-enabled/default':
-  ensure => link,
-  target => '/etc/nginx/sites-available/default',
-  require => File['/etc/nginx/sites-available/default'],
-}
-
-# Remove the default server block symlink (if it exists)
-file { '/etc/nginx/sites-enabled/000-default':
-  ensure => absent,
-}
-
-# Ensure Nginx service is running and enabled
-service { 'nginx':
-  ensure  => 'running',
-  enable  => true,
-  require => [
-    Package['nginx'],
-    File['/etc/nginx/sites-enabled/default'],
-  ],
-}
-
+include nginx
