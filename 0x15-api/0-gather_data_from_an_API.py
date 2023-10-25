ad674759a4afb6386ef76c906007e8fe643667f2
Ismael-ux1 <ismaelabdulahi1@gmail.com>
2023-10-25 10:58:37 -0700

    updated Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,26 +4,19 @@ import requests
 import sys
 
 
-def main():
-    # Define the base URL for the JSONPlaceholder API
-    base_url = "https://jsonplaceholder.typicode.com/"
+def get_employee_todo_progress(employee_id):
+    base_url = 'https://jsonplaceholder.typicode.com'
 
-    # Get the user data by making a GET request to the user endpoint
-    user_id = sys.argv[1]  # Assuming sys.argv[1] contains the user ID
-    user_info = requests.get(f"{base_url}users/{user_id}").json()
+    user_info = requests.get(f'{base_url}/users/{employee_id}').json()
+    todos = requests.get(f'{base_url}/todos?userId={employee_id}').json() 
+    completed_tasks = [task['title'] for task in todos if task['completed']]
+    total_tasks = len(todos)
+    employee_name = user_info.get('name', 'Unknown')
 
-    # Get the user's todos by making a GET request to the todos endpoint
-    todos = requests.get(f"{base_url}todos", params={"userId": user_id}).json()
-
-    # Extract completed tasks
-    completed_tasks = [task["title"] for task in todos if task["completed"]]
-
-    # Print the user's name and the number of completed tasks
-    print(f"{user_info['name']} completed {len(completed_tasks)}/{len(todos)} tasks:")
-
-    # Print the titles of completed tasks
-    [print(f"\t{task}") for task in completed_tasks]
+    print(f'Employee {employee_name} is done with tasks({len(completed_tasks)}/{total_tasks}):')
+    [print(f' {task}') for task in completed_tasks]
 
 
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    employee_id = int(sys.argv[1])
+    get_employee_todo_progress(employee_id)
