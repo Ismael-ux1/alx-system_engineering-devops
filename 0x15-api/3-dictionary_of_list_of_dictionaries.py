59d957691a6c7417c63cd27040921d8b3653e771
Ismael-ux1 <ismaelabdulahi1@gmail.com>
2023-10-25 18:37:56 -0700

    updated Dictionary of list of dictionaries

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,39 +1,51 @@
 #!/usr/bin/python3
-import requests
+""" Python script to export data in the JSON format """
 import json
-""" Python script to export data in the JSON format. """
+import requests
+import sys
 
 
-def fetch_user_tasks(user_id):
-    # Define the base URL for the JSONPlaceholder API
-    base_url = "https://jsonplaceholder.typicode.com/"
+def get_all_employees_todo_progress():
+    base_url = 'https://jsonplaceholder.typicode.com'
 
-    # Get the user data by making a GET request to the user endpoint
-    user_info = requests.get(f"{base_url}users/{user_id}").json()
+    # Fetch all users information
+    users = requests.get(f'{base_url}/users').json()
 
-    # Get the user's todos by making a GET request to the todos endpoint
-    todos = requests.get(f"{base_url}todos", params={"userId": user_id}).json()
+    # Fetch all todos information
+    todos = requests.get(f'{base_url}/todos').json()
 
-    # Extract completed tasks for the user
-    completed_tasks = [{"username": user_info["name"], "task": task["title"], "completed": task["completed"]} for task in todos]
-
-    return user_id, completed_tasks
+    # Export the data to a JSON file
+    export_to_json(users, todos)
 
 
-def main():
-    all_user_tasks = {}
+def export_to_json(users, todos):
+    filename = 'todo_all_employees.json'
+    with open(filename, mode='w') as json_file:
+        # Create an empty dictionary to store the data
+        data = {}
+        # Loop through the users list
+        for user in users:
+            # Get the user ID and username
+            user_id = user['id']
+            username = user['username']
+            # Create a list to store the tasks of this user
+            tasks = []
+            # Loop through the todos list and filter by user ID
+            for todo in todos:
+                if todo['userId'] == user_id:
+                    # Create a dictionary for each task
+                    task_data = {
+                        "username": username,
+                        "task": todo['title'],
+                        "completed": todo['completed']
+                    }
+                    # Append the task data to the list of tasks
+                    tasks.append(task_data)
+            # Add the user ID and the list of tasks to the data dictionary
+            data[str(user_id)] = tasks
+        # Write the data to the JSON file with indentation
+        json.dump(data, json_file, indent=None)
 
-    # List of user IDs,
-    user_ids = [1, 2, 3, 4, 5]  # Example user IDs
 
-    for user_id in user_ids:
-        user_id, completed_tasks = fetch_user_tasks(user_id)
-        all_user_tasks[user_id] = completed_tasks
-
-    # Export all user tasks to a JSON file
-    with open("todo_all_employees.json", mode='w') as json_file:
-        json.dump(all_user_tasks, json_file, indent=4)
-
-
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    get_all_employees_todo_progress()
